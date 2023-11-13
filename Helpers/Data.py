8f93f3dace6af80b32458f8946d2376439f2dc10
Peter DeVries <psdevr6@gmail.com>
2023-11-12 20:39:09 -0600

    Removed global data file and removed the need for manual pixel locations

--- Helpers/Data.py
+++
@@ -1,4 +0,0 @@
-DATA_FILE = {
-    'width': 2560,
-    'height': 1440
-}
--- ReactionTime/ReactionTime.py
+++ ReactionTime/ReactionTime.py
@@ -1,27 +1,17 @@
 from Helpers.ScreenGrabber import *
-from Helpers.Data import DATA_FILE
 import pyautogui
 
-# Define the RGB values for red and green
-# Windows
-red_color = [206, 38, 54]
-green_color = [75, 219, 106]
-width = DATA_FILE['width']
-height = DATA_FILE['height']
+# Define the RGB values for red and green for both mac and windows (values might be different based on machine)
+windows_red_color = [206, 38, 54]
+windows_green_color = [75, 219, 106]
+mac_red_color = [189, 56, 60]
+mac_green_color = [119, 216, 119]
+width = 2560
+height = 1440
 
-# Positions here: x and y are the target position for the color pixel and click
-# extra_monitor_offset needs to be set if an additional monitor is used.
-x, y = 485, 533
-extra_monitor_offset = 0
-
-
-# mac
-# red_color = [189, 56, 60]
-# green_color = [119, 216, 119]
-# width = 1440
-# height = 900
-# x, y = 346, 390
-# extra_monitor_offset = 0
+# Locate pixel location on screen
+res = pyautogui.locateOnScreen("Reaction_Time_Test.png")
+x, y = res.left, res.top
 
 # Instantiates the ScreenGabber class which utilizes mss for fastest times possible
 screen_grabber = ScreenGrabber(0, 0, width, height, -1)
@@ -34,13 +24,13 @@ def reaction_time():
         current_color = screen_grabber.get_pixel_color(x, y)
 
         # Check if pixel is red
-        if current_color == red_color:
+        if current_color == windows_red_color or current_color == mac_red_color:
             while True:
                 # Wait until changes to green
                 screen_grabber.take_screen_shot()
                 current_color = screen_grabber.get_pixel_color(x, y)
-                if current_color == green_color:
-                    pyautogui.click(x + extra_monitor_offset, y)
+                if current_color == windows_green_color or current_color == mac_green_color:
+                    pyautogui.click(x, y)
                     break
 
 
